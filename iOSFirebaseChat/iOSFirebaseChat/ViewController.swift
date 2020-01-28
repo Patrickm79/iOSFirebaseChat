64ad06adae6c2916480232995545dc9a7f92800f
Patrick Millet <lohs79seahawk@gmail.com>
2020-01-28 15:21:53 -0600

    Added library for Firebase and Messagekit

---
+++ iOSFirebaseChat/iOSFirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSFirebaseChat
+//
+//  Created by Patrick Millet on 1/28/20.
+//  Copyright © 2020 Patrick Millet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
