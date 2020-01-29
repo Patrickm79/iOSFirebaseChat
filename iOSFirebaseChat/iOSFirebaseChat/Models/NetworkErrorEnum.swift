9c45bc83f67b6e3dad30254628b5fe9e78ba4fe4
Patrick Millet <lohs79seahawk@gmail.com>
2020-01-29 14:17:41 -0600

    MVP

---
+++ iOSFirebaseChat/iOSFirebaseChat/Models/NetworkErrorEnum.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkErrorEnum.swift
+//  iOSFirebaseChat
+//
+//  Created by Patrick Millet on 1/28/20.
+//  Copyright © 2020 Patrick Millet. All rights reserved.
+//
+
+import Foundation
